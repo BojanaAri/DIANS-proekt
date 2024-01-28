caa6b122a4c1f5b1e76f45872157b466c2a586a4
Bojana <bojanaarizankovska1@gmail.com>
2024-01-28 23:36:45 +0100

    MicroServices Finished

--- Domasno4/tech prototype/dians/src/main/java/com/dians/model/exceptions/PasswordDoNotMatchException.java
+++
@@ -1,7 +0,0 @@
-package com.dians.model.exceptions;
-
-public class PasswordDoNotMatchException extends Exception{
-    public PasswordDoNotMatchException() {
-        super("Passwords do not match");
-    }
-}
