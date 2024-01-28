f03b1a648cd14b1f28f37d6871ac12a6be23fa5f
martinakuzmanovska <m.kuzmanovska2002.2002@gmail.com>
2024-01-28 18:48:17 +0100

    deployment branch

--- Domasno3/tech prototype/dians/src/main/java/com/dians/model/exceptions/InvalidArgumentExceptions.java
+++
@@ -1,7 +0,0 @@
-package com.dians.model.exceptions;
-
-public class InvalidArgumentExceptions extends Exception{
-    public InvalidArgumentExceptions() {
-        super("Invalid arguments!");
-    }
-}
